26873dcc9a6fb9494b7ec3af8e58c236f541da47
Devanshukoli <99028806+Devanshukoli@users.noreply.github.com>
2023-03-04 12:08:54 +0530

    Update removeStringSpace.js

--- removeStringSpace.js
+++ removeStringSpace.js
@@ -1,1 +1,36 @@
+/* Question ; 
+Simple, remove the spaces from the string, then return the resultant string.
+*/
 
+   
+// method 1 :  via regex + replace() method 
+fucntion removeSpace(x) {
+  // here, note that trim() method only trim the given string and not actually modify it. BUT replace () method actually MODIFTY the string.
+  return x.repalce('/ /g', ' ')
+}
+
+// method 2 :  for loops
+function removeSpace(x) {
+  let newStr = ''// define first new empty string
+  // for loop
+  for (const char of x) {
+    if (char !== ' ') newStr += char
+  }
+  return newStr
+}
+
+// method 3 : using replace + join
+function noSpace(x){return x.split(' ').join('')}
+
+// method 4 
+function noSpace(x) {
+  return x
+    .split('') // first it will split the array and make it string
+    .filter((el) => el.trim().length) // it will filter it
+    .join(''); // now it will join the string without any space
+}
+
+// method 5 : via using match() method
+function noSpace(x){
+return x.match(/\S/g).join('');
+}
