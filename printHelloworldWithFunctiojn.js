309b33f2d197cb6f1e542c1a07592ad48d3a65e8
Devanshukoli <99028806+Devanshukoli@users.noreply.github.com>
2023-03-18 15:42:19 +0530

    Add files via upload

---
+++ printHelloworldWithFunctiojn.js
@@ -0,0 +1,22 @@
+// Write a function "greet" that returns "hello world!"
+// 1.
+// const greet = function() {
+//   return "hello world!"
+// }
+
+// console.log(greet());
+
+// 2. classical way of declaring a  function
+// function greet() {
+//   return "hello world!"
+// }
+
+// console.log(greet());
+
+// 3. async function call
+const greet = () => {
+ 
+  return "hello world!"
+}
+
+console.log(greet());
