477c61fd83c3e29e87aeba5fd1dafb2f283a8865
Devanshukoli <kolidevanshu02@gmail.com>
2023-09-12 16:49:19 +0530

    <update> : added one liner code that usese egex in it, and also put some comment in my try code, for better understanding.

--- Regex_validate_PIN_code(7 kyu).js
+++ Regex_validate_PIN_code(7 kyu).js
@@ -11,15 +11,21 @@ Examples (Input --> Output)
 
 // My Try code
 function validatePIN(pin) {
-    
+    // first check if the entered pin length is 4 or 6.
     if (pin.length === 4 || pin.length === 6) {
+        // now go through for loop for checking if the entred pin only consists of `digits`.
         for (let i = 0; i < pin.length; i++) {
             if (isNaN(parseInt(pin))) {
-                return false
+                return false // if not digits
             }
         }
-        return true
+        return true // otherwise `true`
     } else {
         return false
     }
+}
+
+// method 1 : using `regex`.
+function validatePIN(pin) {
+    return /^(\d{4}|\d{6})$/.test(pin)
 }
