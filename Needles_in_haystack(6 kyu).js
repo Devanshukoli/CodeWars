32205da9eb250d8f26ca676e7ec3f3f0851dd90b
Devanshukoli <kolidevanshu02@gmail.com>
2023-09-18 16:37:32 +0530

    <add>: Needles_in_haystack(6 kyu).js file, in which I have learned how to fetch something from object that contais key;value pair

---
+++ Needles_in_haystack(6 kyu).js
@@ -0,0 +1,63 @@
+/*
+Find the needle(s) in a haystack by creating a function that returns all properties (recursively) which contains the needle (string).
+
+Return value should be a sorted array.
+
+Example:
+
+var obj = {
+  site: "Codewars",
+  description: "Lorem ipsum dolor sit...",
+  obj2: {
+    str: "Yeah, Codewars!",
+    num: 123,
+    obj3: {
+      something: "Ph'nglui mglw'nafh Codewars R'lyeh wgah'nagl fhtagn. Gotha fm'latgh h'gof'nn, geb chtenff"
+    }
+  }
+};
+var results = search(obj, "Codewars"); //results = ["obj2.obj3.something", "obj2.str", "site"]
+*/
+
+// My Try code.
+function search(haystack, needle) {
+  const results = [];
+
+  // Helper function to traverse the object properties
+  function traverse(obj, path = '') {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key];
+        const newPath = path ? path + '.' + key : key;
+
+        if (typeof value === 'string' && value.includes(needle)) {
+          results.push(newPath);
+        }
+
+        if (typeof value === 'object') {
+          traverse(value, newPath);
+        }
+      }
+    }
+  }
+
+  traverse(haystack);
+
+  // Sort and return the results array
+  return results.sort();
+}
+
+// Example usage:
+var obj = {
+  site: "Codewars",
+  description: "Lorem ipsum dolor sit...",
+  obj2: {
+    str: "Yeah, Codewars!",
+    num: 123,
+    obj3: {
+      something: "Ph'nglui mglw'nafh Codewars R'lyeh wgah'nagl fhtagn. Gotha fm'latgh h'gof'nn, geb chtenff"
+    }
+  }
+};
+var results = search(obj, "Codewars");
+console.log(results); // Output: ["obj2.obj3.something", "obj2.str", "site"]
