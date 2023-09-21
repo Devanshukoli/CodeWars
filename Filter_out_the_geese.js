c0cc22d7ee4a5d1565205a7813d42e1375572dc3
Devanshukoli <kolidevanshu02@gmail.com>
2023-09-21 15:01:32 +0530

    <update>: Just typeos and stuff

--- Filter_out_the_geese.js
+++ Filter_out_the_geese.js
@@ -17,10 +17,10 @@ The elements in the returned array should be in the same order as in the initial
 
 // My Try code.
 function gooseFilter(birds) {
-    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
 
-    // return an array containing all of the strings in the input array except those that match strings in geese
-    return birds.filter((item) => !geese.includes(item))
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  return birds.filter((item) => !geese.includes(item))
 };
 
 console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]))
