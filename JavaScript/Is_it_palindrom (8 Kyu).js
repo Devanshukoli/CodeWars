9d93d0b4c710f74fe3b02302516f1276c7743ec2
Devanshukoli <kolidevanshu02@gmail.com>
2024-06-26 11:19:26 +0530

    In this kata, I had to make function in js which check whether the given string is palindrom or not.

---
+++ JavaScript/Is_it_palindrom (8 Kyu).js
@@ -0,0 +1,14 @@
+/*
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+
+*/
+
+function checkPalindrom(str) {
+    // return str == str.split('').reverse().join('');
+    console.log(str.toLowerCase() == str.split('').reverse().join('').toLowerCase())
+}
+
+checkPalindrom('Hello') // print : false
