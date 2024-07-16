a2b6e27ea874876222982567c69cfbc5a74eab04
Devanshukoli <kolidevanshu02@gmail.com>
2024-07-16 11:55:39 +0530

    8 Kyu kata
    In this kata, I used by default .reduce() method of js. and then bit of logic from my-side and able to solve thr problem as well.

---
+++ JavaScript/Sum_of_positive (8 Kyu).js
@@ -0,0 +1,25 @@
+/*
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+*/
+
+function positiveSum(arr) {
+    return arr.reduce((sum, num) => num > 0 ? sum + num : sum, 0);
+}
+
+
+// Other methods.
+function positiveSum(arr) {
+    var total = 0;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            total += arr[i];
+        }
+    }
+    return total;
+}
