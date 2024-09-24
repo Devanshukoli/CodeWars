265af6f2d89a95cc25e39005d69a391cfc5d0172
Devanshukoli <kolidevanshu02@gmail.com>
2024-09-24 11:12:22 +0530

    8 Kyu Kata
    In this kata, I just used switch statemnt, but in answers learnd something intersting, like using array accessing to use it to my leverage.

---
+++ JavaScript/8_Kyu/Switch_it_up.js
@@ -0,0 +1,28 @@
+/*
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+
+*/
+
+function switchItUp(num) {
+    switch (num) {
+        case 1: return 'One'
+        case 2: return 'Two'
+        case 3: return 'Three'
+        case 4: return 'Four'
+        case 5: return 'Five'
+        case 6: return 'Six'
+        case 7: return 'Seven'
+        case 8: return 'Eight'
+        case 9: return 'Nine'
+        default: break;
+    }
+}
+
+// Other methods...
+switchItUp = n => ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][n]
