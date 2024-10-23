03c8980afe661d44a4e44e136deb4ba4e319fd25
Devanshukoli <kolidevanshu02@gmail.com>
2024-10-23 14:27:43 +0530

    8 Kyu Kata
    In this kata, I used regex to solve the problem.

---
+++ JavaScript/8_Kyu/Simple_validation_of_username_a_regex.js
@@ -0,0 +1,14 @@
+/*
+
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+
+*/
+
+function validateUsr(username) {
+  return /^[a-z0-9]{4,16}$/.test(username);
+}
