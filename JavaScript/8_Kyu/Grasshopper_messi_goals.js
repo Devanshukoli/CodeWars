4e13db9aa30fb9d01e59d9db5ef4231208aa1419
Devanshukoli <kolidevanshu02@gmail.com>
2024-11-08 11:20:54 +0530

    8 Kyu Kata
    In this kata, I just followed the instruction. and its done.

---
+++ JavaScript/8_Kyu/Grasshopper_messi_goals.js
@@ -0,0 +1,22 @@
+/*
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+*/
+
+var laligaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+var totalGoals = laligaGoals + championsLeagueGoals + copaDelReyGoals;
+console.log(totalGoals);
