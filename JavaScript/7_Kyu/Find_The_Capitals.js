e04cfe48ee775701223fab1b187c346c18d151f7
Devanshukoli <kolidevanshu02@gmail.com>
2025-01-02 17:21:26 +0530

    7 Kyu Kata
    In this kata, I did instruction first,Then used split() method to convert letters into array,  Then used map function to loop through letter, Then used if-else statement to check uppercase. and then filter out any undefined letter.

--- JavaScript/7_Kyu/Find_The_Capitals.js
+++ JavaScript/7_Kyu/Find_The_Capitals.js
@@ -4,3 +4,29 @@ Write a function that takes a single non-empty string of only lowercase and uppe
 "CodEWaRs" --> [0,3,4,6]
 
 */
+
+function capitals(word) {
+    // first we split the word into an array.
+    let splitedWords = word.split('');
+
+    // then we use the map function to iterate over the array and return the index of the capital letters.
+    return splitedWords.map((letter, index) => {
+        // here then we check if the given letter is uppercase, if it is we return the index of the letter.
+        if (letter === letter.toUpperCase()) {
+            return index;
+        }
+    }).filter((index) => index !== undefined); // then we filter out the undefined values.
+
+}
+
+console.log(capitals('CodEWaRs')) // [0,3,4,6]
+
+
+// Other Methods.
+function capitals(word) {
+    let caps = [];
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].toUpperCase() == word[i]) caps.push(i);
+    }
+    return caps;
+};
